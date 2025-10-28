71a576c988bf00a4dce4f450233c716dff82149c
PoistJ <157406020+PoistJ@users.noreply.github.com>
2025-10-28 03:09:10 -0400

    Created the home, shop, and cart components and setup routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App.jsx";
+import Home from "./Home.jsx";
+import Shop from "./Shop.jsx";
+import Cart from "./Cart.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "home",
+    element: <Home />,
+  },
+  {
+    path: "shop",
+    element: <Shop />,
+  },
+  {
+    path: "cart",
+    element: <Cart />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
